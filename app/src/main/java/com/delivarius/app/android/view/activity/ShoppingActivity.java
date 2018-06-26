575313e5a776803dee64831cb82deaf1a8e86574
Roniceli Moura <roniceli@info.ufrn.br>
2018-06-26 15:44:43 -0300

    changes on ShoppingActivity

--- app/src/main/java/com/delivarius/app/android/view/activity/ShoppingActivity.java
+++ app/src/main/java/com/delivarius/app/android/view/activity/ShoppingActivity.java
@@ -1,4 +1,12 @@
 package com.delivarius.app.android.view.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 public class ShoppingActivity extends DelivariusActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
